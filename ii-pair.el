aa6e342e28a552067f2d7099cf1e10686f6792bc
Hippie Hacker <hh@ii.coop>
2021-05-24 13:37:31 +1200

    Initial import of ii-pair emacs modules as their own set.

---
+++ ii-pair.el
@@ -0,0 +1,29 @@
+;;; ii-pair.el --- description -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2021 ii friend
+;;
+;; Author: ii friend <http://github/hh>
+;; Maintainer: ii friend <ii*ii.ii>
+;; Created: May 24, 2021
+;; Modified: May 24, 2021
+;; Version: 0.0.1
+;; Keywords:
+;; Homepage: https://github.com/hh/ii-pair
+;; Package-Requires: ((emacs 27.2) (cl-lib "0.5"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;  description
+;;
+;;; Code:
+
+;; Our pairing environment moostly integrates these modules:
+
+(require 'iterm)
+(require 'osc52)
+(require 'ob-tmate)
+
+(provide 'ii-pair)
+;;; ii-pair.el ends here
